39974cdf22e24588685d7b608b22e770325849b6
NaoriYchiha <lolollb770@gmail.com>
2025-03-16 12:01:32 +0200

    Changed a DB name, network name and name of backend part

--- Betting_CW/backend/src/main/java/com/lba/docker/Betting_CW.java
+++ Betting_CW/backend/src/main/java/com/lba/docker/Betting_CW.java
@@ -1,13 +1,7 @@
 package com.lba.docker;
 
-import com.lba.docker.entity.Product;
-import com.lba.docker.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Betting_CW {
--- Betting_CW/backend/src/main/java/com/lba/docker/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lba.docker.repository;
-
-import com.lba.docker.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
