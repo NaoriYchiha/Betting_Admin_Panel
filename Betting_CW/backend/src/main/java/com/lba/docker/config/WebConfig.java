8d94a0ca32a2ee77c1b7718550318c2def928bdd
NaoriYchiha <lolollb770@gmail.com>
2025-03-16 11:55:48 +0200

    Added security configuration for API handling

---
+++ Betting_CW/backend/src/main/java/com/lba/docker/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.lba.docker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Разрешаем CORS для всех API
+                .allowedOrigins("http://localhost:3000") // Разрешаем только запросы с этого адреса
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешенные HTTP методы
+                .allowedHeaders("*") // Разрешаем все заголовки
+                .allowCredentials(true); // Разрешаем использование куки
+    }
+}
