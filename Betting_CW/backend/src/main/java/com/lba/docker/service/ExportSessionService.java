7f9572544a9aff6cfd5aab1ceee05a1625c1d27d
Bohdan Lytvynovych <lolollb770@gmail.com>
2025-06-02 23:11:16 +0300

    Added a method for export to xlsx

--- Betting_CW/backend/src/main/java/com/lba/docker/service/ExportSessionService.java
+++ Betting_CW/backend/src/main/java/com/lba/docker/service/ExportSessionService.java
@@ -1,11 +1,16 @@
 package com.lba.docker.service;
 
 import com.lba.docker.entity.ExportSession;
+import com.lba.docker.entity.Odds;
+import com.lba.docker.entity.SportsEvent;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ExportSessionService {
     ExportSession save(ExportSession exportSession);
     List<ExportSession> findByUserId(Long userId);
     List<ExportSession> findAll();
+
+    byte[] exportEventsAndOddsToExcel(List<SportsEvent> events, List<Odds> oddsList) throws IOException;
 }
