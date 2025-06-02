4ba84d6151aa905cf393f3077501627f69f4a8de
Bohdan Lytvynovych <lolollb770@gmail.com>
2025-06-02 23:11:05 +0300

    Added a method for import from xlsx

--- Betting_CW/backend/src/main/java/com/lba/docker/service/ImportSessionService.java
+++ Betting_CW/backend/src/main/java/com/lba/docker/service/ImportSessionService.java
@@ -1,6 +1,7 @@
 package com.lba.docker.service;
 
 import com.lba.docker.entity.ImportSession;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface ImportSessionService {
     ImportSession save(ImportSession importSession);
     List<ImportSession> findByUserId(Long userId);
     List<ImportSession> findAll();
+    void importFromExcel(MultipartFile file);
 }
